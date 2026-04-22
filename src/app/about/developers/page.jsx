82f43bdee43150d5ed962ac005b4160b3b42dc89
programming hero <meherun2002@gmail.com>
2026-04-22 22:17:52 +0600

    nexted folder created

---
+++ src/app/about/developers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DevelopersPage = () => {
+    return (
+        <div>
+            <h2>This is developer</h2>
+        </div>
+    );
+};
+
+export default DevelopersPage;
